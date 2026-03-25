7d6797ba17f2805e30ca7ad494c2042a1f08b1b8
Brandon Gui <brandongui123alt@gmail.com>
2026-03-25 17:14:16 +0800

    Made many improvements
    - Created FileStats dataclass so that get_file_stats can return it and
      not a dictionary. This improves type checking because errors will show
      up during linting if using a field that doesn't exist.
    
    - Added accepting stdin so that we can pipe outputs of other commands
      into this program. For this, I made `file` argument optional.
    
    - Updated get_file_stats to take in streams instead of file paths.
    
    - When displaying stats from stdin, no file name is shown after the
      numbers, mirroring `wc` behaviour.
    
    - Update program to give lines, words and bytes count if no flags are
      given.
    
    - Created generator to put strings of field names of the FileStats
      dataclass whenever the corresponding flag is true. This allows use
      of getattr to look up the value of a field given its name.
    
    - Added typing to the program to catch bugs early.

--- wcpy.py
+++ wcpy.py
@@ -1,4 +1,15 @@
 import argparse
+import sys
+
+from dataclasses import dataclass
+from typing import BinaryIO
+
+@dataclass 
+class FileStats:
+    bytes: int
+    lines: int
+    words: int
+    chars: int
 
 def main():
     parser = argparse.ArgumentParser(
@@ -9,58 +20,65 @@ def main():
     parser.add_argument('-l', '--lines', action='store_true', help='Counts the number of lines.', dest='is_counting_lines')
     parser.add_argument('-w', '--words', action='store_true', help='Counts the number of words.', dest='is_counting_words')
     parser.add_argument('-m', '--chars', action='store_true', help='Counts the number of characters.', dest='is_counting_chars')
-    parser.add_argument('file', help='The file to read.')
+    parser.add_argument('file', nargs='?', default='-', help='The file to read. If omitted or is "-", read from standard input.')
 
     args = parser.parse_args()
 
-    file_stats = get_file_stats(args.file)
-    stats_to_show = []
-
-    # handle no flags given
-    if not args.is_counting_lines and not args.is_counting_words and not args.is_counting_bytes:
-        stats_to_show.append(file_stats['lines'])
-        stats_to_show.append(file_stats['words'])
-        stats_to_show.append(file_stats['bytes'])
+    if args.file == '-':
+        stream = sys.stdin.buffer
+        display_name = ''
+        close_after = False
     else:
-        if args.is_counting_lines:
-            stats_to_show.append(file_stats['lines'])
-        
-        if args.is_counting_words:
-            stats_to_show.append(file_stats['words'])
+        stream = open(args.file, 'rb')
+        display_name = args.file
+        close_after = True
 
-        if args.is_counting_chars:
-            stats_to_show.append(file_stats['chars'])
+    try:    
+        file_stats = get_file_stats(stream)
+    finally:
+        if close_after:
+            stream.close()
 
-        if args.is_counting_bytes:
-            stats_to_show.append(file_stats['bytes'])
+    stats_to_show: list[str] = []
+
+    flags = (args.is_counting_lines, args.is_counting_words, args.is_counting_bytes, args.is_counting_chars)
+    if not any(flags):
+        fields = ('lines', 'words', 'bytes')
+    else:
+        # iterates pairs and each time flag is True, this generator yields name
+        fields = tuple(
+            name for name, flag in (
+                ('lines', args.is_counting_lines),
+                ('words', args.is_counting_words),
+                ('chars', args.is_counting_chars),
+                ('bytes', args.is_counting_bytes)
+            ) if flag
+        )
+    
+    # using getattr to pull the value in the field with a specific name
+    stats_to_show = [str(getattr(file_stats, field_name)) for field_name in fields]
 
     # need to convert each item in the array to a string so that after each string a space is added
-    print(' '.join([str(value) for value in stats_to_show]), args.file)
+    print(' '.join([str(value) for value in stats_to_show]), display_name)
     
 
-def get_file_stats(path_to_file) -> dict[str, int]:
+def get_file_stats(stream: BinaryIO) -> FileStats:
     byte_count = 0
     line_count = 0
     word_count = 0
     char_count = 0
 
-    with open(path_to_file, 'rb') as file:
-        for line in file:
-            byte_count += len(line)
+    for line in stream:
+        byte_count += len(line)
 
-            # if we're in CR or LF, we won't find any CRLF, so we get the correct amount as usual
-            # if we're in CRLF, inclusion-exclusion principle helps us get the correct amount
-            line_count += line.count(b'\n') + line.count(b'\r') - line.count(b'\r\n')
-            
-            word_count += len(line.split())
-            char_count += len(line.decode('utf-8', errors='ignore'))
+        # if we're in CR or LF, we won't find any CRLF, so we get the correct amount as usual
+        # if we're in CRLF, inclusion-exclusion principle helps us get the correct amount
+        line_count += line.count(b'\n') + line.count(b'\r') - line.count(b'\r\n')
+        
+        word_count += len(line.split())
+        char_count += len(line.decode('utf-8', errors='ignore'))
 
-    return {
-        'bytes': byte_count,
-        'lines': line_count,
-        'words': word_count,
-        'chars': char_count
-    }
+    return FileStats(byte_count, line_count, word_count, char_count)
 
 if __name__ == '__main__':
     main()
